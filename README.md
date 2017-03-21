cca9fce9a1ffd72726f7fd4ad8c9d420ba9bd091
Alex Britcliffe <alexanderbritcliffe@gmail.com>
2017-03-21 16:25:01 -0400

    last commit before merge

--- README.md
+++ README.md
@@ -3,4 +3,4 @@
 
 Definitely not a Twitter clone.
 
-The purpose of this project is to practice using Grunt to compress our files including SASS JShint index.html and karma.  
+The purpose of this project is to practice using Grunt to compress our files including SASS, JShint, html, jquery and karma. 
--- gruntfile.js
+++ gruntfile.js
@@ -6,10 +6,19 @@ module.exports = function configureGrunt (gruntConfig) {
 
     clean: [ 'build/' ],
 
+    jsHint:{
+      source:{
+        options:{
+          jshintrc:'.jshintrc'
+        },
+        files:{
+          src:['src/**/*.js']
+        }
+      }
+    },
+
     copy: {
-
       copythehtml: {
-
         files: [
           {
             cwd: 'src/',
@@ -18,61 +27,58 @@ module.exports = function configureGrunt (gruntConfig) {
             expand: true
           }
         ]
+      },
+      copyJavaScript: {
+        files: [
+          {
+            cwd:'src/js',
+            src: ['app.js'],
+            dest: 'build/js/',
+            expand: true
+          }
+        ]
+      },
+
+      copyJquerry: {
+        files: [
+          {
+            cwd: 'node_modules/jquery/dist',
+            src:['jquery.js'],
+            dest:'build/js/vendor',
+            expand: true
+          }
+        ]
       }
     },
-    copyJS: {
-      files: [
-        {
-          cwd:'src/',
-          src: ['**/*.js'],
-          dest: 'build/',
-          expand: true
-        }
-      ]
-    },
-
-    copyJquerry: {
-      files: [
-        {
-          cwd: 'node_modules/jquery/dist'
-
-        }
-      ]
-    },
-
-    jsHint: 
 
     sass: {
-      all: {
-
-        files: {
-
+      allSASS:{
+        files:{
           'build/style.css' : 'src/sass/main.scss'
         }
       }
-
     },
 
-    karma: {
-      all:
+    karma:{
+      testunits:{
         options: {
           frameworks: [ 'mocha', 'chai' ],
           browsers: [ 'Chrome' ],
           singleRun: true,
           files: [
             'src/**/*.js',
-            'node_modules/fetch-mock/es5/client-browserified.js',
+            'node_modules/sinon/pkg/sinon-2.0.0.js',
             'test/specs/**/*.js'
-        ]
+          ]
+        }
       }
     }
-  }
 
 
   });
 
-require('load-grunt-tasks')(gruntConfig);
+  require('load-grunt-tasks')(gruntConfig);
 
-gruntConfig.registerTask ('build', [ 'jshint', 'karma', 'clean', 'copy', 'sass' ]);
+  gruntConfig.registerTask ('build', [ 'jshint', 'karma', 'clean', 'copy', 'sass' ]);
 
 };
